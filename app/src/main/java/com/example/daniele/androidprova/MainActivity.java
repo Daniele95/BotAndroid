fc2d2629e4a912419aa95877e8a5b33445291f1e
Daniele Gamba <danielee.gamba@gmail.com>
2018-12-15 14:39:04 +0100

    initzio

---
+++ app/src/main/java/com/example/daniele/androidprova/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.daniele.androidprova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /** Called when the user taps the Send button */
+   // fun sendMessage(view: View) {
+ //       // Do something in response to button
+  //  }
+
+
+}
